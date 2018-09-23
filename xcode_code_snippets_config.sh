0931eef2e4be138331841a4ad2ddf56558c10b65
aaronge <aaronge@tencent.com>
2018-09-23 15:29:14 +0800

    sh: change git source url

--- xcode_code_snippets_config.sh
+++ xcode_code_snippets_config.sh
@@ -2,8 +2,8 @@
 echo 'del source if exist'
 rm -fr ~/Downloads/code_snippets
 
-echo 'clone code_snippets source from http://git.code.oa.com/aaronge/code_snippets.git to downloads folder'
-git clone http://git.code.oa.com/aaronge/code_snippets.git ~/Downloads/code_snippets
+echo 'clone code_snippets source from https://github.com/gezhixin/code_snippets.git to downloads folder'
+git clone https://github.com/gezhixin/code_snippets.git ~/Downloads/code_snippets
 
 echo 'move snippets file to config folder'
 if [ ! -d "~/Library/Developer/Xcode/UserData/CodeSnippets" ]; then
